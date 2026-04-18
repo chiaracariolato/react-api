ad0fa8f8286e64bdf1e15f4b00c1905aa60f5e00
chiaracariolato <chiaracariolato@gmail.com>
2026-04-18 17:22:44 +0200

    Feat: Milestone 3

--- src/components/CardAttore.jsx
+++ src/components/CardAttore.jsx
@@ -5,17 +5,25 @@ const CardAttore = (props) => {
     return (
         <div className="card actor-card">
             <img className="card-img-top card-img" src={attore.image} alt={attore.name} />
-            <div className="card-body">
-                <h5 className="card-title">{attore.name}</h5>
+            <div className="card-body mb-3">
+                <div className="my-3">
+                    <h3 className="card-title mb-0">{attore.name}</h3>
+                    <span> {attore.birth_year} - {attore.death_year
+                        ? `${attore.death_year}`
+                        : 'Oggi'}</span>
+                </div>
                 <p className="card-text">{attore.biography}</p>
             </div>
             <ul className="list-group list-group-flush">
-                <li className="list-group-item">Anno di nascita: {attore.birth_year}</li>
-                <li className="list-group-item"> {attore.death_year
-                    ? `Anno di morte: ${attore.death_year}`
-                    : 'In vita'}</li>
                 <li className="list-group-item">Nazionalità: {attore.nationality}</li>
                 <li className="list-group-item">Premi: {attore.awards}</li>
+                <li className="list-group-item"> Film noti:
+                    <ul>
+                        {(attore.known_for || attore.most_famous_movies || []).map((film, index) => (
+                            <li key={index}>{film}</li>
+                        ))}
+                    </ul>
+                </li>
             </ul>
         </div >
     )
